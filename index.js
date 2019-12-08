f4cc1e8fd1eae4353bfee681287ccd1bff084c33
Gordey Doronin <g.doronin@gmail.com>
2019-12-08 17:03:56 +0700

    Get master ref, create new ref, then create PR

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ async function run() {
     const token = core.getInput('repo-token', { required: true });
     const octokit = new github.GitHub(token);
 
-    await octokit.pulls.create({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        title: 'Test PR created by action',
-        head: `apple-version-history-${uuid.v4()}`,
-        base: 'master',
-    });
+    const { owner, repo } = github.context.repo;
+
+    const masterRef = await octokit.git.getRef({ owner, repo, ref: 'refs/heads/master' });
+
+    const newRefName = `refs/heads/apple-version-history-${uuid.v4()}`;
+    await octokit.git.createRef({ owner, repo, ref: newRefName, sha: masterRef.data.object.sha })
+
+    await octokit.pulls.create({ owner, repo, title: 'Test PR created by action', head: newRefName, base: 'master', });
 }
 
 run();
