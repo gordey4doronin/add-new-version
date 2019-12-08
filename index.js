837fd1a58408243baf245773e8d278ef90e8d84e
Gordey Doronin <g.doronin@gmail.com>
2019-12-08 16:47:58 +0700

    Add missing parameters

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const uuid = require('uuid');
 
 async function run() {
     const token = core.getInput('repo-token', { required: true });
@@ -8,7 +9,9 @@ async function run() {
     await octokit.pulls.create({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
-        title: 'Test PR created by action'
+        title: 'Test PR created by action',
+        head: `apple-version-history-${uuid.v4()}`,
+        base: 'master',
     });
 }
 
