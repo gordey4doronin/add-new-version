856e00fcde1770f781047235025b5079152f1925
Gordey Doronin <g.doronin@gmail.com>
2019-12-09 23:03:58 +0700

    Creating PR works

--- index.js
+++ index.js
@@ -8,71 +8,64 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = require("@actions/core");
-const github = require("@actions/github");
-const uuid_1 = require("uuid");
-const node_fetch_1 = require("node-fetch");
-const util_1 = require("util");
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+const uuid_1 = __importDefault(require("uuid"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const local = process.env.NODE_ENV == 'local';
         const token = !local ? core.getInput('repo-token', { required: true }) : process.env.TOKEN;
-        const octokit = new github.GitHub(token);
+        const client = new github.GitHub(token);
         const { owner, repo } = !local ? github.context.repo : { owner: 'gordey4doronin', repo: 'apple-version-history' };
         core.debug(`owner: ${owner}`);
         core.debug(`repo: ${repo}`);
-        const masterRef = yield octokit.git.getRef({ owner, repo, ref: 'heads/master' });
-        const masterSha = masterRef.data.object.sha;
-        core.debug(`masterSha: ${masterSha}`);
-        const masterCommit = yield octokit.git.getCommit({ owner, repo, commit_sha: masterSha });
-        const masterTreeSha = masterCommit.data.tree.sha;
+        const masterRef = yield client.git.getRef({ owner, repo, ref: 'heads/master' });
+        core.debug(`masterSha: ${masterRef.data.object.sha}`);
+        const fileName = 'ios-version-history.json';
+        const data = yield fetchData(fileName);
+        modifyData(data);
+        const stringified = JSON.stringify(data, replacer, 4).replace(/\"\[/g, '[').replace(/\]\"/g, ']').replace(/\\"/g, '"').replace(/\\"/g, '"') + '\n';
+        const content = Buffer.from(stringified).toString('base64');
+        const file = yield client.repos.getContents({ owner, repo, path: fileName });
         const newRefName = `apple-version-history-${uuid_1.default.v4()}`;
         core.debug(`newRefName: ${newRefName}`);
-        yield octokit.git.createRef({ owner, repo, ref: `refs/heads/${newRefName}`, sha: masterRef.data.object.sha });
-        const fileName = 'ios-version-history.json';
-        const content = yield fetchContent();
-        modifyContent(content);
-        core.debug(`content: ${content}`);
-        const newBlob = yield octokit.git.createBlob({ owner, repo, content: JSON.stringify(content, null, 4) });
-        const newBlobSha = newBlob.data.sha;
-        core.debug(`newBlobSha: ${newBlobSha}`);
-        const newTree = yield octokit.git.createTree({
-            owner, repo, base_tree: masterTreeSha, tree: [{
-                    path: fileName,
-                    mode: '100644',
-                    type: 'blob',
-                    sha: newBlob.data.sha
-                }]
-        });
-        const newTreeSha = newTree.data.sha;
-        core.debug(`newTreeSha: ${newTreeSha}`);
-        const newCommit = yield octokit.git.createCommit({ owner, repo, message: 'Committed via Octokit!', tree: newTree.data.sha, parents: [masterSha] });
-        const newCommitSha = newCommit.data.sha;
-        core.debug(`newCommitSha: ${newCommitSha}`);
-        try {
-            yield octokit.git.updateRef({ owner, repo, ref: `refs/${newRefName}`, sha: newCommit.data.sha });
-        }
-        catch (error) {
-            core.error(util_1.default.inspect(error));
-        }
-        core.debug(`ref updated`);
-        yield octokit.pulls.create({ owner, repo, title: 'Test PR created by action', head: newRefName, base: 'master', });
+        yield client.git.createRef({ owner, repo, ref: `refs/heads/${newRefName}`, sha: masterRef.data.object.sha });
+        yield client.repos.createOrUpdateFile({ owner, repo, content, message: 'Committed via Octokit!', path: fileName, branch: newRefName, sha: file.data.sha });
+        yield client.pulls.create({ owner, repo, title: 'Test PR created by action', head: newRefName, base: 'master', });
         core.debug(`pr created`);
     });
 }
-function fetchContent() {
+function replacer(_, value) {
+    if (Array.isArray(value)) {
+        return `[ ${value.map((x) => `"${x}"`).join(', ')} ]`;
+    }
+    else {
+        return value;
+    }
+}
+function fetchData(fileName) {
     return __awaiter(this, void 0, void 0, function* () {
-        const response = yield node_fetch_1.default('https://raw.githubusercontent.com/gordey4doronin/apple-version-history/master/ios-version-history.json');
+        const response = yield node_fetch_1.default(`https://raw.githubusercontent.com/gordey4doronin/apple-version-history/master/${fileName}`);
         return response.json();
     });
 }
-function modifyContent(content) {
+function modifyData(data) {
     const newVersion = '11.4.777';
     const newBuild = 'OMG';
     const newContent = {};
     newContent[newVersion] = [newBuild];
-    Object.assign(content['iOS 11.4.x'], newContent);
-    return content;
+    Object.assign(data['iOS 11.4.x'], newContent);
 }
 run();
--- index.ts
+++ index.ts
@@ -1,80 +1,57 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { ReposGetContentsResponseItem } from '@octokit/rest';
 import uuid from 'uuid';
 import fetch from 'node-fetch';
-import util from 'util';
 
 async function run() {
     const local = process.env.NODE_ENV == 'local';
     const token = !local ? core.getInput('repo-token', { required: true }) : process.env.TOKEN;
-    const octokit = new github.GitHub(token!);
+    const client = new github.GitHub(token!);
     const { owner, repo } = !local ? github.context.repo : { owner: 'gordey4doronin', repo: 'apple-version-history' };
 
     core.debug(`owner: ${owner}`);
     core.debug(`repo: ${repo}`);
 
-    const masterRef = await octokit.git.getRef({ owner, repo, ref: 'heads/master' });
-    const masterSha = masterRef.data.object.sha;
-    core.debug(`masterSha: ${masterSha}`);
-
-    const masterCommit = await octokit.git.getCommit({ owner, repo, commit_sha: masterSha });
-    const masterTreeSha = masterCommit.data.tree.sha;
-
-    const newRefName = `apple-version-history-${uuid.v4()}`;
-    core.debug(`newRefName: ${newRefName}`);
-
-    await octokit.git.createRef({ owner, repo, ref: `refs/heads/${newRefName}`, sha: masterRef.data.object.sha })
+    const masterRef = await client.git.getRef({ owner, repo, ref: 'heads/master' });
+    core.debug(`masterSha: ${masterRef.data.object.sha}`);
 
     const fileName = 'ios-version-history.json';
+    const data = await fetchData(fileName);
+    modifyData(data);
 
-    const content = await fetchContent();
-    modifyContent(content);
-    core.debug(`content: ${content}`);
+    const stringified = JSON.stringify(data, replacer, 4).replace(/\"\[/g, '[').replace(/\]\"/g, ']').replace(/\\"/g, '"').replace(/\\"/g, '"') + '\n';
+    const content = Buffer.from(stringified).toString('base64');
+    const file = await client.repos.getContents({ owner, repo, path: fileName });
+    const newRefName = `apple-version-history-${uuid.v4()}`;
+    core.debug(`newRefName: ${newRefName}`);
+    await client.git.createRef({ owner, repo, ref: `refs/heads/${newRefName}`, sha: masterRef.data.object.sha });
+    await client.repos.createOrUpdateFile({ owner, repo, content, message: 'Committed via Octokit!', path: fileName, branch: newRefName, sha: (file.data as ReposGetContentsResponseItem).sha });
 
-    const newBlob = await octokit.git.createBlob({ owner, repo, content: JSON.stringify(content, null, 4) });
-    const newBlobSha = newBlob.data.sha;
-    core.debug(`newBlobSha: ${newBlobSha}`);
-
-    const newTree = await octokit.git.createTree({
-        owner, repo, base_tree: masterTreeSha, tree: [{
-            path: fileName,
-            mode: '100644',
-            type: 'blob',
-            sha: newBlob.data.sha
-        }]
-    });
-    const newTreeSha = newTree.data.sha;
-    core.debug(`newTreeSha: ${newTreeSha}`);
-
-    const newCommit = await octokit.git.createCommit({ owner, repo, message: 'Committed via Octokit!', tree: newTree.data.sha, parents: [masterSha] });
-    const newCommitSha = newCommit.data.sha;
-    core.debug(`newCommitSha: ${newCommitSha}`);
-
-    try {
-        await octokit.git.updateRef({ owner, repo, ref: `refs/${newRefName}`, sha: newCommit.data.sha });
-    } catch (error) {
-        core.error(util.inspect(error));
-    }
-    core.debug(`ref updated`);
-
-    await octokit.pulls.create({ owner, repo, title: 'Test PR created by action', head: newRefName, base: 'master', });
+    await client.pulls.create({ owner, repo, title: 'Test PR created by action', head: newRefName, base: 'master', });
     core.debug(`pr created`);
 }
 
-async function fetchContent() {
-    const response = await fetch('https://raw.githubusercontent.com/gordey4doronin/apple-version-history/master/ios-version-history.json');
+function replacer(_: string, value: any): any {
+    if (Array.isArray(value)) {
+        return `[ ${value.map((x) => `"${x}"`).join(', ')} ]`;
+    } else {
+        return value;
+    }
+}
+
+async function fetchData(fileName: string): Promise<Object> {
+    const response = await fetch(`https://raw.githubusercontent.com/gordey4doronin/apple-version-history/master/${fileName}`);
     return response.json();
 }
 
-function modifyContent(content) {
+function modifyData(data: Object): void {
     const newVersion = '11.4.777';
     const newBuild = 'OMG';
     const newContent = {};
 
     newContent[newVersion] = [newBuild];
-    Object.assign(content['iOS 11.4.x'], newContent);
-
-    return content;
+    Object.assign(data['iOS 11.4.x'], newContent);
 }
 
 run();
